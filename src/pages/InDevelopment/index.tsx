d0aa3019fed00ac4dbe8e88c360e5ca7e11eb830
Erikrmb <erikrmb@gmail.com>
2023-04-29 00:56:33 -0400

    Testando mudancas apos implementacao do load balancer

---
+++ src/pages/InDevelopment/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./InDevelopment.module.scss"
+
+export default function InDevelopment(){
+    return(
+        <main>
+            <div className={styles.InDevelopment__image}></div>
+            <p>TEste</p>
+        </main>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+import InDevelopment  from './pages/InDevelopment';
 
 export default function AppRouter(){
     return(
         <main>
             <Router>
                 <Routes>
-                    <Route path='/' element={<Home/>}>
-
+                    <Route path='/' >    
+                        <Route index element={<Home/>}></Route>
+                        <Route path='portfolio' element={<InDevelopment/>}></Route>
                     </Route>
                 </Routes>
             </Router>
